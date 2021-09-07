e801ffb4d7d1388d73adb90ca9685ab25c6eaa04
CJ <cj@bluebird.dev>
2021-09-07 16:28:27 +0400

    Init

---
+++ scripts/generate-pdf.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const { PDFDocument } = require('pdf-lib');
+const fs = require('fs');
+
+(async () => {
+  const [,,name] = process.argv;
+
+  if (!name) {
+    console.warn('No name supplied!');
+    process.exit(-1);
+  }
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/');
+  await page.emulateMediaType('screen');
+  const puppeteerPdf = await page.pdf({
+    path: './react.pdf', // path (relative to CWD) to save the PDF to.
+    printBackground: true,// print background colors
+    format: 'a4',
+  });
+  await browser.close();
+
+  // Give the buffer to pdf-lib
+  const pdfDoc = await PDFDocument.load(puppeteerPdf)
+  pdfDoc.setTitle(name, { showInWindowTitleBar: true })
+  const pdfBytes = await pdfDoc.save()
+
+  // write to disk
+  await fs.writeFileSync(`${name}.pdf`, pdfBytes)
+})();
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import picture from './picture';
+
+import person from './cj';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='pdf'>
+        <div className="background-picture">{picture}</div>
+
+        <h1>{person.name}</h1>
+
+        <h2>{person.titles.join(', ')}</h2>
+
+        <div className="info">
+          <div><span className="emoji">☎</span> {person.info.phone}</div>
+          <div><span className="emoji">✉</span> {person.info.email}</div>
+          <div><span className="emoji">🌐</span> {person.info.linkedin}</div>
+          <div><span className="emoji">⚲</span> {person.info.location}</div>
+        </div>
+
+        <div className="split">
+          <div>
+            <h3>Summary</h3>
+            <p>{person.summary}</p>
+
+            <h3>Work Experience</h3>
+            {person.workExperience.map((we, i) => (
+              <div key={i}>
+                <h4>{we.title}</h4>
+                <div className="align">
+                  <div className="highlight">{we.company}</div>
+                  <div className="dates">{we.dates}</div>
+                </div>
+                <ul>
+                  {we.bulletPoints.map((bp, j) => <li key={j}><div className="dot" /><div>{bp}</div></li>)}
+                </ul>
+              </div>
+            ))}
+          </div>
+          <div>
+            <h3>Skills</h3>
+            <div className="group">{person.skills.map(s => <div key={s}>{s}</div>)}</div>
+
+            <h3>Technologies</h3>
+            <div className="group">{person.technologies.map(s => <div key={s}>{s}</div>)}</div>
+
+            <h3>Education</h3>
+            <div className="education">
+              {person.education.map((e, i) => (
+                <div key={i}>
+                  <h4>{e.name}</h4>
+                  {e.degrees.map(d => (
+                    <div key={d.name}>
+                      <div className="align">
+                        <div>{d.name}</div>
+                        <div className="dates">{d.dates}</div>
+                      </div>
+                      <div className="highlight">{d.grades}</div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+
+            <h3>Life experiences</h3>
+            <ul className="life-experiences">
+              {person.lifeExperiences.map((le, i) => (
+                  <li key={i}><div className="dot" /><div>{le}</div></li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
